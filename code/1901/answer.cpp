aa2f35b283682e0a7bb6be17f792c9ac3c2f62de
Jason-Tom <429295712@qq.com>
2016-04-24 22:23:02 +0800

    数组测试ok

---
+++ code/1901/answer.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+int main(void)
+{
+	int a[1024],b[1024];
+	printf("请输入数组a：\n");
+	scanf("%s",&a);
+	printf("请输入数组b：\n");
+	scanf("%s",&b);
+	printf("数组a：%s\n",a);
+	printf("数组b：%s\n",b);
+	return 0;
+}
