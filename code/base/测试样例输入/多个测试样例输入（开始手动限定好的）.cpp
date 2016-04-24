7f75cb28e3482c2aac1d1ff6ba8550402c26b8f2
Jason-Tom <429295712@qq.com>
2016-04-24 20:40:47 +0800

    多测试用例，实现输入数字限定个数

--- code/base/测试样例输入/多个测试样例输入（开始手动限定好的）.cpp
+++ code/base/测试样例输入/多个测试样例输入（开始手动限定好的）.cpp
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-int x=0;
+int x=0,T=0;
 int main(void){
-//    freopen("test.txt","r",stdin);
-     printf("the num is %d \n", x);
+    freopen("test.txt","r",stdin);
+//     printf("the num is %d \n", x);
 //   输出语句，左边指定输出类型，可以加上修饰文字，要对应好格式，右边对应输出变量，不用加上&。
-//    int T;
-//    printf("请输入要输入的数字个数：\n");
-//    scanf("%d",&T);
-//
-//    while(T!=0){
-//        printf("请输入数字：\n");
-//        scanf("%d",x);
-//        printf("%d\n",&x);
-//        T--;
-//    }
+    int T;
+    printf("请输入要输入的数字个数：\n");
+    scanf("%d",&T);
+    while(T!=0){
+        printf("请输入数字：\n");
+        scanf("%d",&x);
+//       输入语句别忘了输入&
+        printf("%d\n",x);
+        T--;
+    }
     return 0;
 }
