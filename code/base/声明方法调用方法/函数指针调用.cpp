1d0b55a3960cf157e9e42b52eedc6d326dab5387
Jason-Tom <429295712@qq.com>
2016-04-24 21:38:54 +0800

    引用型函数可以改变全局变量

---
+++ code/base/声明方法调用方法/函数指针调用.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// 函数声明，放在主函数之前，表示有这个函数可以使用，具体定义函数可以放在后面
+void swap(int *x, int *y);
+
+int main ()
+{
+	// 局部变量声明
+	int a = 100;
+	int b = 200;
+
+	cout << "交换前，a 的值：" << a << endl;
+	cout << "交换前，b 的值：" << b << endl;
+
+	/* 调用函数来交换值
+	 * &a 表示指向 a 的指针，即变量 a 的地址
+	 * &b 表示指向 b 的指针，即变量 b 的地址
+	 */
+	swap(&a, &b);
+
+	cout << "交换后，a 的值：" << a << endl;
+	cout << "交换后，b 的值：" << b << endl;
+
+	return 0;
+}
+// 函数定义
+void swap(int *x, int *y)
+{
+	int temp;
+	temp = *x;	/* 保存地址 x 的值 */
+	*x = *y;		/* 把 y 赋值给 x */
+	*y = temp;	/* 把 x 赋值给 y */
+
+	return;
+}
