b314d3818f533c2f470aeddbd3fe97e9d6520d12
Jason-Tom <429295712@qq.com>
2016-04-25 09:04:32 +0800

    输出语句，输出数据数据类型对应

--- code/1901/answer.cpp
+++ code/1901/answer.cpp
@@ -3,19 +3,27 @@
 #include<string.h>
 int main(void)
 {
-	char a[1024];
+	char a[1024],b[1024];
 	// 字符串数据类型要对应好字符串
 	int c=0,d=0;
-	string b = "Hello";
 	printf("请输入数组a：\n");
 	scanf("%s",a);
 	printf("请输入数组b：\n");
 	scanf("%s",b);
 	c=strlen(a);
-	d=strlen(d);
-	printf("数组a：%s\n",a);
-	printf("数组b：%s\n",b);
-	printf("数组a长度：%d\n",c);
-	printf("数组b长度：%d\n",d);
+	d=strlen(b);
+	for (int i = 0; i < c; ++i)
+	{
+		printf("第%d个字符是：%c\n",i+1,a[i]);
+		// 数据类型对应好，后面的变量可以使用运算表达式 
+		for (int j = 0; j < d; ++i)
+		{
+			/* code */
+		}
+	}
+	// printf("数组a：%s\n",a);
+	// printf("数组b：%s\n",b);
+	// printf("数组a长度：%d\n",c);
+	// printf("数组b长度：%d\n",d);
 	return 0;
 }
--- code/base/字符串/base.cpp
+++ code/base/字符串/base.cpp
@@ -3,13 +3,6 @@
 #include<string.h>
 int main(void)
 {
-    char a[1024];
-    // 字符串数据类型要对应好字符串
-    int c=0,d=0;
-    printf("请输入数组a：\n");
-    scanf("%s",a);
-    c=strlen(a);
-    printf("数组a：%s\n",a);
-    printf("数组a长度：%d\n",c);
+    printf("asfsasfs\n");
     return 0;
 }
