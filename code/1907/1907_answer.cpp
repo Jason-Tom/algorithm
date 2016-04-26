c3c8d3a6f8acff72739ff664c1d096e380fdbe8f
Jason-Tom <429295712@qq.com>
2016-04-26 20:32:53 +0800

    数组定义和初始化ok

--- code/1907/1907_answer.cpp
+++ code/1907/1907_answer.cpp
@@ -4,7 +4,13 @@ int main() {
 	int max[100], v = 0, n_kind = 0;
 	memset(max, 0, sizeof(max));
 	scanf("%d%d", &v, &n_kind);
-	// int weight[]
+	int weight[1024],value[1024];
+	memset(weight,0,sizeof(weight));
+	memset(value,0,sizeof(value));
     printf("%d%d\n",v,n_kind);
+    for (int i = 0; i < 1024; ++i)
+    {
+    	printf("%d %d\n",weight[i],value[i] );
+    }
 	return 0;
 }
