556309c6beff088a5b9ea41faff4c64705cc4e02
Jason-Tom <429295712@qq.com>
2016-04-26 20:19:02 +0800

    多测试用例ok

--- code/1907/1907_answer.cpp
+++ code/1907/1907_answer.cpp
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	int max[100];
+	int max[100],t=0,kind=0;
 	memset(max, 0, sizeof(max));
-	for (int i = 0; i < 100; ++i)
+	scanf("%d%d",&t,&kind);
+	while(t--)
 	{
-		printf("%d\n",max[i] );
+		// printf("%d\n",t );
 	}
+	// for (int i = 0; i < 100; ++i)
+	// {
+	// 	printf("%d\n",max[i] );
+	// }
 	return 0;
 }
