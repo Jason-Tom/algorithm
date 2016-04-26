38820ed5fcd9a37f45dfcbb2bfb867781848c346
Jason-Tom <429295712@qq.com>
2016-04-26 20:25:52 +0800

    输入数据测试ok

--- code/1907/1907_answer.cpp
+++ code/1907/1907_answer.cpp
@@ -1,16 +1,10 @@
 #include<stdio.h>
 #include<string.h>
-int main(){
-	int max[100],t=0,kind=0;
+int main() {
+	int max[100], v = 0, n_kind = 0;
 	memset(max, 0, sizeof(max));
-	scanf("%d%d",&t,&kind);
-	while(t--)
-	{
-		// printf("%d\n",t );
-	}
-	// for (int i = 0; i < 100; ++i)
-	// {
-	// 	printf("%d\n",max[i] );
-	// }
+	scanf("%d%d", &v, &n_kind);
+	// int weight[]
+    printf("%d%d\n",v,n_kind);
 	return 0;
 }
