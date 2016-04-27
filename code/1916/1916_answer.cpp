c64c002f806dbaa6d15c4bde0784106e3b69af79
Jason-Tom <429295712@qq.com>
2016-04-27 23:25:44 +0800

    排序ok

--- code/1916/1916_answer.cpp
+++ code/1916/1916_answer.cpp
@@ -8,11 +8,11 @@ int main()
 	// printf("asfdasdfa\n");
 	open
 	freopen("test.txt", "r", stdin);
-	int t = 0,l=0;
+	int t = 0, l = 0;
 	scanf("%d", &t);
 	while (t--)
 	{
-		int n = 0, star[1024], end[1024], num = 0,temp=0;
+		int n = 0, star[1024], end[1024], num = 0, temp = 0, temp_max = 0;
 		scanf("%d", &n);
 		memset(star, 0, sizeof(star));
 		memset(end, 0, sizeof(end));
@@ -23,22 +23,39 @@ int main()
 		}
 		// sort(star, star + n);
 		// sort(end, end + n);
-		for (int i = 1; i <= n; ++i)
+		for (int i = 0; i < n; ++i)
 		{
-			// printf("%d\n",i);
-			if (temp<=star[i])
+			for (int j = i + 1; j < n; ++j)
 			{
-				temp=end[i];
-				num++;
-				// printf("%d\n",i);
+				if (end[i] > end[j])
+				{
+					temp_max = end[i];
+					end[j] = end[i];
+					end[j] = temp_max;
+				}
+
 			}
 		}
+		// for (int i = 0; i < n; ++i)
+		// {
+		// 	printf("%d\n", end[i]);
+		// }
+		// for (int i = 1; i <= n; ++i)
+		// {
+		// 	// printf("%d\n",i);
+		// 	if (temp <= star[i])
+		// 	{
+		// 		temp = end[i];
+		// 		num++;
+		// 		// printf("%d\n",i);
+		// 	}
+		// }
 		// for (int i = 1; i <= n; ++i)
 		// {
 		// 	printf("%d %d\n", star[i], end[i]);
 		// }
 		// printf("%d\n",t);
 		l++;
-		printf("Case %d: %d\n", l,num );
+		printf("Case %d: %d\n", l, num );
 	}
 }
