d9acc7077e052be50e56513ee66590b8ea6717e3
Jason-Tom <429295712@qq.com>
2016-04-27 08:14:40 +0800

    10000调试ok 是边界出错

---
+++ code/10000/10000.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	// freopen("test.txt", "r", stdin);
+	int max[100], value[1024], temp[1024], weight[1024], v = 0, time = 0, i = 0;
+	scanf("%d%d", &v, &time);
+	int count = time;
+	memset(max, 0, sizeof(max));
+	// memset(temp, 0, sizeof(temp));
+	memset(weight, 0, sizeof(weight));
+	memset(value, 0, sizeof(value));
+	// printf("%d%d\n",v,time);
+	while (time--)
+	{
+		scanf("%d%d", &value[time], &weight[time]);
+	}
+	// printf("%d%d\n", value[time], weight[time]);
+	for (i = 0; i < v + 1; ++i)
+	{
+		memset(temp, 0, sizeof(temp));
+		for (int j = 0; j < count; ++j)
+		{
+			if ((i - weight[j]) >= 0)
+			{
+				temp[j] = max[i - weight[j]] + value[j];
+				// 数组中括号里面可以放表达式
+				// printf("%d\n", temp[j] );
+			}
+			else
+			{
+				;
+			}
+		}
+		for (int j = 0; j < count; ++j)
+		{
+
+			if (max[i] <= temp[j])
+			{
+				max[i] = temp[j];
+				// printf("%d\n", max[i] );
+			}
+		}
+	}
+	// printf("%d\n", max[i] );
+	printf("%d\n", max[v] );
+	return 0;
+}
--- code/10000/1907_answer.cpp
+++ code/10000/1907_answer.cpp
@@ -1,19 +1,51 @@
 #include<stdio.h>
 #include<string.h>
-#define swith if(1)
+#define open if(1)
 int main() {
-	swith
+	open
 	freopen("test.txt", "r", stdin);
-	int max[100], value[1024], weight[1024], v = 0, time = 0;
+	int max[100], value[1024], temp[1024], weight[1024], v = 0, time = 0, i = 0;
 	scanf("%d%d", &v, &time);
+	int count = time;
 	memset(max, 0, sizeof(max));
+	// memset(temp, 0, sizeof(temp));
 	memset(weight, 0, sizeof(weight));
 	memset(value, 0, sizeof(value));
 	// printf("%d%d\n",v,time);
 	while (time--)
 	{
-		scanf("%d%d", &value[time], &weight[time]);
-		printf("%d%d\n", value[time], weight[time]);
+		i = 0;
+		scanf("%d%d", &value[i], &weight[i]);
+		// printf("%d%d\n", value[time], weight[time]);
+		for (i = 0; i < v + 1; ++i)
+		{
+			memset(temp, 0, sizeof(temp));
+			for (int j = 0; j < count; ++j)
+			{
+				if ((i - weight[j]) >= 0)
+					// 少了个等号
+				{
+					temp[j] = max[i - weight[j]] + value[j];
+					// 数组中括号里面可以放表达式
+					// printf("%d\n", temp[j] );
+				}
+				else
+				{
+					;
+				}
+			}
+			for (int j = 0; j < count; ++j)
+			{
+
+				if (max[i] <= temp[j])
+				{
+					max[i] = temp[j];
+					// printf("%d\n", max[i] );
+				}
+			}
+		}
+		// printf("%d\n", max[i] );
 	}
+	printf("%d\n", max[v] );
 	return 0;
 }
