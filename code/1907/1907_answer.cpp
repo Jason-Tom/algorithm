cd04d4d212712f6b09659cfeb05aa45a4d331b32
Jason-Tom <429295712@qq.com>
2016-04-27 18:11:05 +0800

    输入数据矫正

--- code/1907/1907_answer.cpp
+++ code/1907/1907_answer.cpp
@@ -5,17 +5,19 @@
 int main() {
 	swith
 	freopen("test.txt", "r", stdin);
-	int max[100], value[1024], weight[1024], v = 0, n = 0;
-	scanf("%d%d", &v, &n);
+	int max[100], value[1024], weight[1024], v = 0, n = 0, t = 0;
 	memset(max, 0, sizeof(max));
 	memset(weight, 0, sizeof(weight));
 	memset(value, 0, sizeof(value));
-	// printf("%d%d\n",v,n);
-	while (n--)
+	scanf("%d",&t);
+	while (t--)
 	{
-		printf("%d\n", mymax(9, 7) );
-		// scanf("%d%d",&value[n],&weight[n]);
-		// printf("%d%d\n",value[n],weight[n]);
+		scanf("%d%d", &n, &v);
+		for (int i = 0; i < n; ++i)
+		{
+			scanf("%d%d", &weight[i], &value[i]);
+			printf("%d%d\n", weight[i], value[i]);
+		}
 	}
 	return 0;
 }
