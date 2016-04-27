52fa57b34ac1ac53ff965e7706feb66a98938d88
Jason-Tom <429295712@qq.com>
2016-04-27 18:20:31 +0800

    双重测试ok

--- code/1907/1907_answer.cpp
+++ code/1907/1907_answer.cpp
@@ -5,18 +5,28 @@
 int main() {
 	swith
 	freopen("test.txt", "r", stdin);
-	int max[100], value[1024], weight[1024], v = 0, n = 0, t = 0;
+	int max[100], value[1024], weight[1024], c = 0, n = 0, t = 0;
 	memset(max, 0, sizeof(max));
 	memset(weight, 0, sizeof(weight));
 	memset(value, 0, sizeof(value));
-	scanf("%d",&t);
+	scanf("%d", &t);
 	while (t--)
 	{
-		scanf("%d%d", &n, &v);
+		scanf("%d%d", &n, &c);
 		for (int i = 0; i < n; ++i)
 		{
 			scanf("%d%d", &weight[i], &value[i]);
-			printf("%d%d\n", weight[i], value[i]);
+			// printf("%d%d\n", weight[i], value[i]);
+			for (int j = 0; j < n; ++j)
+				// 物体
+			{
+				for (int k = c-1; k >= 0; --k)
+					// 质量
+				{
+					// printf("j:%d k:%d\n", j, k);
+				}
+				// printf("\n");
+			}
 		}
 	}
 	return 0;
