7d91388be984c078538d9cb6d4ef3cf4546f8443
Jason-Tom <429295712@qq.com>
2016-04-27 22:06:47 +0800

    多测试用例ok

--- code/1916/1916_answer.cpp
+++ code/1916/1916_answer.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #define open if(1)
-int main(){
-	printf("asfdasdfa\n");
+int main()
+{
+	// printf("asfdasdfa\n");
+	int t = 0;
+	scanf("%d",&t);
+	while(t--)
+	{
+		printf("%d\n",t);
+	}
 }
