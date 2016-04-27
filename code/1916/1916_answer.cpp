814c32152bdc4aef1e16cc1e300451fc201ac8f1
Jason-Tom <429295712@qq.com>
2016-04-27 22:11:01 +0800

    循环数组输入ok

--- code/1916/1916_answer.cpp
+++ code/1916/1916_answer.cpp
@@ -8,6 +8,14 @@ int main()
 	scanf("%d",&t);
 	while(t--)
 	{
-		printf("%d\n",t);
+		int n=0,star[1024],end[1024];
+		scanf("%d",&n);
+		memset(star,0,sizeof(star));
+		memset(end,0,sizeof(end));
+		for (int i = 1; i <= n; ++i)
+		{
+			printf("%d %d\n",star[i],end[i] );
+		}
+		// printf("%d\n",t);
 	}
 }
