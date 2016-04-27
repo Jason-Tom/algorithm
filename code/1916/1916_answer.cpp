269416fefb4056d649bc2b83e1dff40d9d346e5e
Jason-Tom <429295712@qq.com>
2016-04-27 22:18:44 +0800

    sort排序ok

--- code/1916/1916_answer.cpp
+++ code/1916/1916_answer.cpp
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
+using namespace std;
 #define open if(1)
 int main()
 {
 	// printf("asfdasdfa\n");
 	open
-	freopen("test.txt","r",stdin);
+	freopen("test.txt", "r", stdin);
 	int t = 0;
-	scanf("%d",&t);
-	while(t--)
+	scanf("%d", &t);
+	while (t--)
 	{
-		int n=0,star[1024],end[1024];
-		scanf("%d",&n);
-		memset(star,0,sizeof(star));
-		memset(end,0,sizeof(end));
+		int n = 0, star[1024], end[1024];
+		scanf("%d", &n);
+		memset(star, 0, sizeof(star));
+		memset(end, 0, sizeof(end));
 		for (int i = 1; i <= n; ++i)
 		{
-			scanf("%d%d",&star[i],&end[i]);
-			printf("%d %d\n",star[i],end[i]);
+			scanf("%d%d", &star[i], &end[i]);
+			// printf("%d %d\n",star[i],end[i]);
+		}
+		sort(star, star + n);
+		sort(end, end + n);
+		for (int i = 1; i <= n; ++i)
+		{
+			printf("%d %d\n", star[i], end[i]);
 		}
 		// printf("%d\n",t);
 	}
