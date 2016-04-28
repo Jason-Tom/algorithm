12ca5149dac46f986fb8f424e0c1d68a9c47bfad
Jason-Tom <429295712@qq.com>
2016-04-28 13:07:31 +0800

    机器数组初始化ok

--- code/1919/1919_answer.cpp
+++ code/1919/1919_answer.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 using namespace std;
 #define open if(1)
-bool complare(int a,int b)
+bool complare(int a, int b)
 {
- return a>b;
+	return a > b;
 }
 int main() {
 	freopen("test.txt", "r", stdin);
@@ -13,19 +13,34 @@ int main() {
 	scanf("%d", &t);
 	while (t--) {
 		int n = 0, m = 0, time[1024];
-		memset(time,0,sizeof(time));
-	// n任务个数，m是机器数量，time每个任务对应的时间
+		memset(time, 0, sizeof(time));
+		// n任务个数，m是机器数量，time每个任务对应的时间
 		scanf("%d%d", &n, &m);
 		for (int i = 0; i < n; ++i)
 		{
-			scanf("%d",&time[i]);
+			scanf("%d", &time[i]);
 			// printf("time:%d \n",time[i]);
 		}
-		sort(time,time+n,complare);
+		sort(time, time + n, complare);
 		// for (int i = 0; i < n; ++i)
 		// {
 		// 	printf("time:%d \n",time[i]);
 		// }
+		int my_max_time = 0;
+		if (n <= m)
+		{
+			my_max_time = time[0];
+		}
+		else
+		{
+			int machine[m];
+			memset(machine,0,sizeof(machine));
+			for (int i = 0; i < m; ++i)
+			{
+				printf("%d\n",machine[i] );
+			}
+		}
+		// printf("%d\n", my_max_time);
 		// printf("%d %d\n", n, m);
 	}
 }
