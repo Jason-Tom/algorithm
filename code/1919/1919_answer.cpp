98298aa8abcd966f03f1c8238801feca71e957e2
Jason-Tom <429295712@qq.com>
2016-04-28 12:44:30 +0800

    数据输入成功

--- code/1919/1919_answer.cpp
+++ code/1919/1919_answer.cpp
@@ -3,10 +3,18 @@
 #define open if(1)
 int main() {
 	freopen("test.txt", "r", stdin);
-	int t = 0, n = 0, m = 0;
+	int t = 0;
 	scanf("%d", &t);
 	while (t--) {
+		int n = 0, m = 0, time[1024];
+		memset(time,0,sizeof(time));
+	// n任务个数，m是机器数量，time每个任务对应的时间
 		scanf("%d%d", &n, &m);
-		printf("%d %d\n", n, m);
+		for (int i = 0; i < n; ++i)
+		{
+			scanf("%d",&time[i]);
+			printf("time:%d \n",time[i]);
+		}
+		// printf("%d %d\n", n, m);
 	}
 }
