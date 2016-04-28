e0ed2f7c41b7389afc1a2b46c201f096b9b9618c
Jason-Tom <429295712@qq.com>
2016-04-28 12:34:56 +0800

    数组最大的最小值ok

--- code/base/base/base.cpp
+++ code/base/base/base.cpp
@@ -1,9 +1,11 @@
-#include<stdio.h>
-#include<iostream>
-#include<string.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
+#define open if(1)
 int x=0,T=0;
 int main(void){
+    open
     freopen("test.txt","r",stdin);
 //     printf("the num is %d \n", x);
 //   输出语句，左边指定输出类型，可以加上修饰文字，要对应好格式，右边对应输出变量，不用加上&。
---
+++ code/base/数组最大最小值/数组最大最小值.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+#define open if(0)
+int x = 0, T = 0;
+int main(void) {
+    open
+    freopen("test.txt", "r", stdin);
+    int a[4] = {2, 4, 1, 3}, mymax = 0, mymin = 0;
+    mymin = a[1];
+    mymax = a[1];
+    for (int i = 0; i < 4; ++i)
+    {
+        if (mymax < a[i])
+        {
+            mymax = a[i];
+        }
+
+        if (mymin > a[i])
+        {
+            mymin = a[i];
+        }
+    }
+    printf("最大值：%d 最小值：%d\n", mymax, mymin );
+    return 0;
+}
