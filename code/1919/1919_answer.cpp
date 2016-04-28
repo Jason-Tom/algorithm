c9bfd7319871a3a16cafeb6167ad16e225e10f5d
Jason-Tom <429295712@qq.com>
2016-04-28 12:37:28 +0800

    n m 输入ok

--- code/1919/1919_answer.cpp
+++ code/1919/1919_answer.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #define open if(1)
-int main(){
-	freopen("test.txt","r",stdin);
-	int t=0;
-	scanf("%d",&t);
-	while(t--){
-		printf("asdfas\n" );
+int main() {
+	freopen("test.txt", "r", stdin);
+	int t = 0, n = 0, m = 0;
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d%d", &n, &m);
+		printf("%d %d\n", n, m);
 	}
 }
