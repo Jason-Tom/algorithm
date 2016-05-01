da6bfd709cab1df47cc3e07b58aa841a918ba9bc
Jason-Tom <429295712@qq.com>
2016-05-01 10:49:13 +0800

    递归函数ok

--- code/base/递归/n!.cpp
+++ code/base/递归/n!.cpp
@@ -2,9 +2,16 @@
 int recursion(int a);
 int main()
 {
-	printf("%d\n",recursion(5));
+	printf("%d\n", recursion(5));
+	// recursion(6);
 	return 0;
 }
-int recursion(int a){
-	return a;
+int recursion(int a) {
+	if (a == 1)
+	{
+		// printf("%d\n", a );
+		return 1;
+	}
+	// printf("%d\n", a );
+	return recursion(a - 1) * a;
 }
