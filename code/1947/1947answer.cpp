1a4ab711432cab83f5c0e91f3d978635d99d1538
Jason-Tom <429295712@qq.com>
2016-05-02 11:43:49 +0800

    1947输入数据ok

--- code/1947/1947answer.cpp
+++ code/1947/1947answer.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+#define myopen if(1)
+#define mynum 10024
+#define mymemset(a) memset(a,0,sizeof(a))
+int mydata[mynum];
 int main()
 {
-	
+	myopen
+	freopen("test.txt", "r", stdin);
+	int n = 0, num = 0;
+	while (scanf("%d", &n) != EOF)
+	{
+		mymemset(mydata);
+		for (int i = 0; i < n; ++i)
+		{
+			scanf("%d", &mydata[i]);
+			printf("%d\n", mydata[i]);
+		}
+	}
 	return 0;
 }
