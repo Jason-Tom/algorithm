f7679674072fa51afa43f50c2146963f1ae95eee
Jason-Tom <429295712@qq.com>
2016-05-02 11:29:26 +0800

    不限定个数多测试用例输入

---
+++ code/base/测试样例输入/不限定个数.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#define max(a,b) a>b?a:b
+#define MAXN 10000
+
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF)
+	{
+		printf("%d\n", n);
+	}
+	return 0;
+}
